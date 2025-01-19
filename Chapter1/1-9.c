7b0c90c7c2bce8cf5a321d08a0a4a50cbbf80149
mohammad <mohammad@icloud.com>
2025-01-19 22:08:57 +1100

    completed solution for 1-9

---
+++ Chapter1/1-9.c
@@ -0,0 +1,32 @@
+/*
+*	Write a program to copy its input to its output, replacing each string of
+*	one or more blanks by a single blank.
+*/
+
+#include <stdio.h>
+
+#define YES 1
+#define NO 0
+
+int main(void) {
+
+	int c;
+
+	// If a space has just been sent to stdout, this will be set to YES,
+	// otherwise will be set to no
+	int placedSpace = NO;
+
+	while((c = getchar()) != EOF) {
+		// If the current char is space, and previous isn't, then can putchar
+		if (c == ' ') {
+			if (placedSpace == NO) {
+				putchar(c);
+				placedSpace = YES;
+			}
+		}
+		else if (c != ' ' ) {
+			putchar(c);
+			placedSpace = NO;
+		}
+	}
+}
