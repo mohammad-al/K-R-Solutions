13c11880ab266c8a8934bee39506e786dbdc4b24
mohammad <mohammad@icloud.com>
2025-02-01 00:40:38 +1100

    completed solution for exercise 1-23. Wrote the remove_comments.c file

---
+++ Chapter1/1-23/remove_comments.c
@@ -0,0 +1,64 @@
+/*
+*	Write a program to remove all comments from a C program. Don't forget to
+*	handle quoted strings and character constants properly. C comments do not
+*	nest.
+*/
+
+#include <stdio.h>
+
+void remove_inline_comment(void);
+void remove_block_comment(void);
+void expand_quotes(void);
+
+int main(void) {
+
+	int c;
+	while ((c = getchar()) != EOF) {
+		// Check for comment
+		if (c == '/') {
+			int d = getchar();
+			if (d == '/') {
+				remove_inline_comment();
+			} else if (d == '*') {
+				remove_block_comment();
+			} else {
+				putchar(c);
+				putchar(d);
+			}
+		// Inside a string literal
+		} else if (c == '"') { 
+			putchar(c);
+			expand_quotes();	
+		} else {
+			putchar(c);
+		}
+	}
+}
+
+void remove_inline_comment(void) {
+	
+	// Keep reading until we have reached the end of the line
+	while (getchar() != '\n') {
+		;
+	}	
+	putchar('\n');
+}
+void remove_block_comment(void) {
+
+	int c = getchar();
+	int d = getchar();
+
+	// Keep reading characters until reached end of comment
+	while (c != '*' || d != '/') {
+		// This makes sure we go character by character and not two at a time
+		c = d;
+		d = getchar();
+	}
+}
+void expand_quotes(void) {
+	int c;
+	while ((c = getchar()) != '"') {
+		putchar(c);
+	}
+	putchar(c);
+}
