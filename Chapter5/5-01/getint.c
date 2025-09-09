e3a9791413d3e3e9a9d133d03189fd02bd3a1afb
mohammad <mohammad@icloud.com>
2025-09-09 16:51:36 +1000

    completed exercise 5-01

---
+++ Chapter5/5-01/getint.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUFSIZE 100
+
+int getint(int *pn);
+char getch(void);
+void ungetch(char c);
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int main(void) {
+
+	int x;
+	if (getint(&x) > 0) {
+		printf("%d\n", x);
+	}
+
+}
+
+int getint(int *pn) {
+
+	int c, sign;
+
+	// Skip whitespace
+	while (isspace(c = getchar())) {
+		;
+	}
+	// Means not an integer.
+	if (!isdigit(c) && c != EOF && c != '-' && c != '+') {
+		ungetch(c);
+		return 0;
+	}
+	// Get this sign
+	sign = (c == '-') ? -1 : 1;
+	// Skip the sign if it is there
+	if (c == '-' || c == '+') {
+		c = getch();	
+	}
+	// Next character should be digit.
+	if (!isdigit(c) && c != EOF) {
+		ungetch(c);
+		return 0;
+	}
+	for (*pn = 0; isdigit(c); c = getch()) {
+		*pn = *pn * 10 + (c - '0');
+	}
+	*pn *= sign;
+	if (c != EOF) {
+		ungetch(c);
+	}
+	// Returns the character after the last digit
+	return c;
+}
+char getch(void) {
+	return bufp > 0 ? buf[--bufp] : getchar();
+}
+void ungetch(char c) {
+	if (bufp < BUFSIZE) {
+		buf[bufp++] = c;	
+	} else {
+		printf("error: buffer is full\n");
+	}
+}
