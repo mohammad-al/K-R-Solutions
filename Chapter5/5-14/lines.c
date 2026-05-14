08f773dc40e65cc92f2d6e68ceeac99e62ba1cbf
Mohammad <mohammad.alabody02@gmail.com>
2026-05-15 00:25:30 +1000

    feat: completed exercise 5-14.

---
+++ Chapter5/5-14/lines.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "alloc.h"
+#include "lines.h"
+#include "string.h"
+
+// Maximum length of any given line
+#define MAXLEN 1000
+
+int readlines(char *lineptr[], int maxlines) {
+	
+	int nlines = 0;
+	int len;
+	char *p;
+	char line[MAXLEN];
+
+
+	// if we read a line, then handle it
+	while ((len = my_getline(line, MAXLEN)) > 0) {
+		if (nlines >= maxlines || (p = alloc(len)) == NULL) {
+			return -1;
+		} else {
+			// Remove the newline character from the line
+			line[len - 1] = '\0';
+			strcpy(p, line);
+			lineptr[nlines++] = p;
+			p += len;
+		}
+	}
+	return nlines;
+}
+
+void writelines(char *lineptr[], int nlines) {
+	
+	int i;
+	for (i = 0; i < nlines; i++) {
+		printf("%s\n", lineptr[i]);
+	}
+}
+
+int my_getline(char line[], int lim) {
+	
+	int i;
+	int c;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+		line[i] = c;
+	}
+	if (c == '\n') {
+		line[i++] = '\n';
+	}
+	line[i] = '\0';
+	return i;
+}
---
+++ Chapter5/5-14/lines.h
@@ -0,0 +1,8 @@
+#ifndef LINES_H
+#define LINES_H
+
+int readlines(char *lineptr[], int maxlines);
+void writelines(char *lineptr[], int nlines);
+int my_getline(char line[], int limit);
+
+#endif
