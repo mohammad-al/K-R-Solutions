04026f20090cb982943d7a31ae29453b115bd3c9
Mohammad <mohammad.alabody02@gmail.com>
2026-05-16 18:53:23 +1000

    feat: finished exercise 5-17

--- Chapter5/5-17/main.c
+++ Chapter5/5-17/main.c
@@ -9,6 +9,8 @@
 
 #define MAXSTORAGE 10000
 
+#define MAXSTR 1000
+
 int numeric = 0;
 int reverse = 1;
 int fold = 0;
@@ -32,7 +34,7 @@ void my_qsort(
 
 int numcmp(char *, char *);
 int my_strcmp(char *, char *);
-
+void get_substring(char *s, char *sub);
 int parse_arguments(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
@@ -98,7 +100,13 @@ void my_qsort(
 
 int numcmp(char *s1, char *s2) {
 	double v1, v2;
-	v1 = atof(s1);
+
+	char string[MAXSTR];
+
+	get_substring(s1, string);
+	v1 = atof(string);
+
+	get_substring(s2, string);
 	v2 = atof(s2);
 	if (v1 < v2) {
 		return -1 * reverse;
@@ -109,16 +117,37 @@ int numcmp(char *s1, char *s2) {
 	}
 }
 int my_strcmp(char *s1, char *s2) {
+
+	int s1_len = strlen(s1);
+	if (field_1 > s1_len || field_2 > s1_len) {
+		printf("error: invalid fields. not within range\n");
+		exit(1);
+	}
+
+	int s2_len = strlen(s2);
+	if (field_1 > s2_len || field_2 > s2_len) {
+		printf("error: invalid fields. not within range\n");
+		exit(1);
+	}
+
+	// Set the start of s1 and s2 when doing comparisons
+	s1 += field_1 - 1;
+	s2 += field_1 - 1;
+
+	// Set the end of s1 and s2 that we need to traverse up to
+	char *end_s1 = field_2 > 0 ? s1 + (field_2 - field_1) : s1 + s1_len;
+	char *end_s2 = field_2 > 0 ? s2 + (field_2 - field_1) : s2 + s2_len;
+
 	
 	// Go character by character and do strcmp manually
-	while (*s1 != '\0' && *s2 != '\0') {
+	while (s1 < end_s1 && s2 < end_s2) {
 
 		// If directory order, skip current characters if they are not alphanum or space
 		if (directory_order) {
-			while (*s1 != '\0' && !isalnum(*s1) && !isspace(*s1)) {
+			while (s1 < end_s1 && !isalnum(*s1) && !isspace(*s1)) {
 				s1++;
 			}
-			while (*s2 != '\0' && !isalnum(*s2) && !isspace(*s2)) {
+			while (s2 < end_s2 && !isalnum(*s2) && !isspace(*s2)) {
 				s2++;
 			}
 		}
@@ -178,3 +207,20 @@ int parse_arguments(int argc, char *argv[]) {
 	return 0;
 }
 
+void get_substring(char *s, char *sub) {
+
+	int final_field = strlen(s);
+	// Means field_2 is beyond the string end
+	if (field_2 > 0 && final_field < field_2) {
+		printf("error: field 2 is too large\n");
+		exit(1);
+	// Update final field to be field_2 if provided
+	} else if (field_2 > 0 && final_field > field_2) {
+		final_field = field_2;
+	}
+	int j = 0;
+	for (int i = field_1; i < final_field; i++, j++) {
+		sub[j] = s[i];
+	}
+	sub[j] = '\0';
+}
